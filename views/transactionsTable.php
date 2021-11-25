f19de87d62699c49ca3199f402c8c916d5e16223
kkedzierski <kamil.kedzierski@onet.com.pl>
2021-11-26 00:31:34 +0100

    add simple css

---
+++ views/transactionsTable.php
@@ -0,0 +1,27 @@
+<table>
+    <thead>
+        <tr>
+            <th>Date</th>
+            <th>Check #</th>
+            <th>Description</th>
+            <th>Amount</th>
+        </tr>
+    </thead>
+    <tbody>
+        <!-- YOUR CODE -->
+    </tbody>
+    <tfoot>
+        <tr>
+            <th colspan="3">Total Income:</th>
+            <td><!-- YOUR CODE --></td>
+        </tr>
+        <tr>
+            <th colspan="3">Total Expense:</th>
+            <td><!-- YOUR CODE --></td>
+        </tr>
+        <tr>
+            <th colspan="3">Net Total:</th>
+            <td><!-- YOUR CODE --></td>
+        </tr>
+    </tfoot>
+</table>
